a3786e91d70ece2676cb46db329fd16cecff340e
carlos <gonzalezcgg1@gmail>
2021-04-23 15:11:53 -0500

    rewrote consumer to be fetchonly.js - still having bug with display in browser.

---
+++ consumer/fetchonly.js
@@ -0,0 +1,24 @@
+let videoTag = document.getElementById("my-video");                             
+let myMediaSource = new MediaSource();                                          
+videoTag.src = URL.createObjectURL(myMediaSource);
+
+myMediaSource.addEventListener("sourceopen", pushToBuffer);
+
+fileLimit = 1000;
+currentRange = 0;
+delta = 200;
+fileURL = "/segments/audio_and_video/segment1.mp4"
+
+function pushToBuffer(){
+    while(currentRange < fileLimit){
+        fetch(fileURL, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "video/mp4",
+                "Content-Range": `bytes ${currentRange}-${currentRange+delta}/${fileLimit}`
+            }
+        })
+
+        currentRange = currentRange + delta;
+    }
+}
