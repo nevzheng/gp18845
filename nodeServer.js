1d1b5e87fe8b71dbb2f957c6320e9a3f38153e07
carlos <gonzalezcgg1@gmail>
2021-04-25 17:30:33 -0500

    added documentation to nodeServer.js

--- nodeServer.js
+++ nodeServer.js
@@ -5,12 +5,12 @@
  * To run the server do:
  * > node nodeServer.js
  *
- * the 4 files to be served by this server can be served by running this server
- * and going to any of the following
- * http://localhost:3000/segment1.mp4
- * http://localhost:3000/segment2.mp4
- * http://localhost:3000/segment3.mp4
- * http://localhost:3000/segment4.mp4
+ * the server will serve any files inside of: consumer/segments/audio_and_video
+ * the relevant URL is:
+ * http://localhost:3000/test.html
+ * which will serve the html file that runs the fetchonly.js script and downloads
+ * all of the segments of the mp4 file that get streamed
+ *
  */
 
 const express = require('express');
